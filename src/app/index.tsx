ebc930ec49168a3eac904844cd0974ac4b72c517
vicmzz-cl <victor.galdino.silva29@escola.pr.gov.br>
2026-05-11 14:07:52 -0300

    Enhance Input components with keyboard type and security

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import { Input } from "@/componentes/input";
 import { Image, StyleSheet, Text, View } from "react-native";
 export default function Index(){
     return(
+    <ScrollView>
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
           <Image
             source={require("/home/escola/TrabalhoFlavia/src/app/assets/castaldi.jpg")}
@@ -10,11 +11,11 @@ export default function Index(){
           <Text style={styles.title}>Programação Mobile.</Text>
           <Text style={styles.subtitle}>Acesse sua conta.</Text>
           <View style={styles.form}>
-            <Input placeholder="E-mail" />
-            <Input placeholder="Senha" />
+            <Input placeholder="E-mail" keyboardType="email-address" />
+            <Input placeholder="Senha" secureTextEntry />
           </View>
         </View>
-
+     </ScrollView>
     );
 }
 
